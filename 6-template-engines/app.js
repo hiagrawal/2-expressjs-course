bb6551c54bae227af3e183225574bc9d3f9bfe7c
hiagrawal <agrawal.hina13@gmail.com>
2021-07-28 16:11:34 +0530

    sharing data across views, installing template engines, installing and implementing pug

--- 6-template-engines/app.js
+++ 6-template-engines/app.js
@@ -5,13 +5,23 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+//app.set can have various paramters that we can set
+//can view all here: http://expressjs.com/en/api.html#app.set
+//here, we are using 'view engine' and 'views'
+//view engine is to set the default template engine to use when to load html/pug file
+//and views to define the folder where all html/pug files are placed, in our case, it is 'views' folder only which is also the default value
+app.set('view engine','pug');
+app.set('views',path.join(__dirname, 'views'));
+//now, will have to create a pug file which will have dynamic content
+//and also, will render the pug file in shop.js instead oh html 
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- 6-template-engines/routes/shop.js
+++ 6-template-engines/routes/shop.js
@@ -3,11 +3,22 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin')
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  console.log('shop.js', adminData.products);
+  //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  //since it is not html file. will not just sendFile but will use render which is a expressjs method to render the pug file
+  //and since path is already given in app.js file, that all html/pug files are in views folder, hence just need to give the file name
+  //and default pug template engine is also defined in app.js file, hence no need to give pug extension in shop file
+  res.render('shop.pug');
 });
 
 module.exports = router;
+
+//this is one way of sharing data - that we got the data in admin.js, exported it and imported in shop.js data
+//now one major disadvantage with this is - since the data is on localhost:3000 server, even if we close the tab and reopen
+//or open in new window, or new browser or in incognito, will always get the data since it is on that 3000 node
+//this data is inherent from the node server and therefore shared across
