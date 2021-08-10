7e689e7bdb98dbffb466009fd13dc5c5303dffd3
hiagrawal <agrawal.hina13@gmail.com>
2021-08-10 12:42:43 +0530

    deleting files and creating pug folder and handlebars folder separately

--- 6-template-engines/util/path.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-
-//module.exports = path.dirname(process.mainModule.filename); //this is in older version
-module.exports = path.dirname(require.main.filename); //this in updated version
-
-//although currently both are working
